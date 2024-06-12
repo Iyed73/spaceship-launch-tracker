81daff5b8763b1491a1c9b604fc737eb6d2bd605
Iyed <iyedabdelli73@gmail.com>
2024-06-13 00:51:18 +0100

    Create LaunchSite, Rocket & RocketLaunch models

--- app/models.py
+++ app/models.py
@@ -1,14 +1,20 @@
 from typing import Optional
-from sqlalchemy import func,  String, Integer, ForeignKey, Table, Column
+from sqlalchemy import func,  String, Integer, ForeignKey, Table, Column, DateTime
 from sqlalchemy.orm import Mapped, mapped_column, Session, relationship, WriteOnlyMapped
 from app import db
 from datetime import datetime, timezone
 from uuid import UUID, uuid4
+from sqlalchemy.ext.declarative import declared_attr
 
 
 class TimestampMixin:
-    created_at: Mapped[datetime] = mapped_column(default=lambda: datetime.now(timezone.utc))
-    updated_at: Mapped[Optional[datetime]] = mapped_column(onupdate=lambda: datetime.now(timezone.utc))
+    @declared_attr
+    def created_at(cls):
+        return Column(DateTime, default=lambda: datetime.now(timezone.utc))
+
+    @declared_attr
+    def updated_at(cls):
+        return Column(DateTime, onupdate=lambda: datetime.now(timezone.utc))
 
 
 class User(db.Model, TimestampMixin):
@@ -23,3 +29,57 @@ class User(db.Model, TimestampMixin):
 
     def __repr__(self):
         return f'{self.role}({self.id.hex}, "{self.username}", {self.email})'
+
+
+class RocketLaunch(db.Model, TimestampMixin):
+    __tablename__ = 'rocket_launches'
+
+    id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
+    mission: Mapped[String] = mapped_column(String(128), index=True)
+    description: Mapped[Optional[str]] = mapped_column(String(1024))
+    launch_timestamp: Mapped[datetime]
+    rocket_id: Mapped[int] = mapped_column(ForeignKey('rockets.id'), index=True)
+    launch_site_id: Mapped[int] = mapped_column(ForeignKey('launch_sites.id'), index=True)
+
+    rocket: Mapped['Rocket'] = relationship(
+        lazy='joined', back_populates='rocket_launches'
+    )
+
+    launch_site: Mapped['LaunchSite'] = relationship(
+        lazy='joined', back_populates='rocket_launches'
+    )
+
+    def __repr__(self):
+        return f"Rocket({self.id.hex},{self.mission})"
+
+
+class Rocket(db.Model, TimestampMixin):
+    __tablename__ = 'rockets'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(64), index=True, unique=True)
+    description: Mapped[Optional[str]] = mapped_column(String(1024))
+    height: Mapped[float]
+    mass: Mapped[float]
+    payload_capacity: Mapped[float]
+    thrust_at_liftoff: Mapped[float]
+
+    rocket_launches: Mapped[list['RocketLaunch']] = relationship(
+        cascade='all, delete-orphan', back_populates='rocket')
+
+    def __repr__(self):
+        return f"Rocket({self.id},{self.name})"
+
+
+class LaunchSite(db.Model, TimestampMixin):
+    __tablename__ = 'launch_sites'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(64), index=True, unique=True)
+    location: Mapped[str] = mapped_column(String(256))
+
+    rocket_launches: Mapped[list['RocketLaunch']] = relationship(
+        cascade='all, delete-orphan', back_populates='launch_site')
+
+    def __repr__(self):
+        return f"LaunchSite({self.id},{self.name})"
--- launchvault.py
+++ launchvault.py
@@ -1,9 +1,9 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import app, db
-from app.models import User
+from app.models import User, LaunchSite
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'so': so, 'sa': sa, 'db': db, 'User': User}
+    return {'so': so, 'sa': sa, 'db': db, 'User': User, 'LaunchSite': LaunchSite}
