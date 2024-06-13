278e7acc13e1a1d30c3b14b3db64e23f8a487b31
Iyed <iyedabdelli73@gmail.com>
2024-06-13 19:08:53 +0100

    Create blueprints & routes for authentication & index

---
+++ app/routes/authentication.py
@@ -0,0 +1,14 @@
+from app.blueprints import authentication_bp as bp
+from app.views.register import RegisterView
+from app.views.login import LoginView
+from app.views.logout import LogoutView
+
+
+bp.add_url_rule("/register",
+                view_func=RegisterView.as_view("register"))
+
+bp.add_url_rule("/log",
+                view_func=LoginView.as_view("login"))
+
+bp.add_url_rule("/logout",
+                view_func=LogoutView.as_view("logout"))
---
+++ app/routes/main.py
@@ -0,0 +1,12 @@
+from app.blueprints import main_bp as bp
+from app.views.index import HomeView
+from flask import redirect, url_for
+
+
+@bp.route("/", methods=["GET"])
+def root():
+    return redirect(url_for("main.index"))
+
+
+bp.add_url_rule("/home",
+                view_func=HomeView.as_view("index"))
