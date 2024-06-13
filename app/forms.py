2323e9a6a0f6548f4f33a9e1a524b1b97206b5b3
Iyed <iyedabdelli73@gmail.com>
2024-06-14 00:29:52 +0100

    Update username validator in registration form

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length, Regexp
 from sqlalchemy import select
 from app import db
 from app.models import User
@@ -15,12 +14,16 @@ class LoginForm(FlaskForm):
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=64)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=64),
+                                                   Regexp(r'^[\w.@+-_]+$', message="Only alphanumeric characters, "
+                                                                                   "dots, '@', '+', '-' and '_' are "
+                                                                                   "allowed for username.")])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=64)])
     password_confirmation = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
+    # todo: validate_username & validate_email are only called if the other validators have passed
     def validate_username(self, username):
         user = db.session.scalar(select(User).where(User.username == username.data))
         if user is not None:
