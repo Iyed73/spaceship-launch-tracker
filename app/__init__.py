257b1f30de8e60690216c44384f6ff11c6d2b261
Iyed <iyedabdelli73@gmail.com>
2024-06-13 18:10:27 +0100

    Integrate LoginManager & Bootstrap in the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bootstrap import Bootstrap5
 
 
 db = SQLAlchemy()
 migrate = Migrate()
+login = LoginManager()
+bootstrap = Bootstrap5()
 
 
 def create_app(test_config=None):
@@ -16,10 +20,13 @@ def create_app(test_config=None):
 
     db.init_app(app)
     migrate.init_app(app, db)
+    login.init_app(app)
+    bootstrap.init_app(app)
 
-    @app.route('/hello')
-    def hello():
-        return 'Rockets Launch Tracker!'
+    from app.routes import authentication, main
+
+    app.register_blueprint(authentication.bp, url_prefix='/authentication')
+    app.register_blueprint(main.bp)
 
     return app
 
