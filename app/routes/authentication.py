5de5c698b5afab745549572de5ad14103af002eb
Iyed <iyedabdelli73@gmail.com>
2024-06-14 07:51:01 +0100

    Add a view for account confirmation

--- app/routes/authentication.py
+++ app/routes/authentication.py
@@ -2,6 +2,7 @@ from app.blueprints import authentication_bp as bp
 from app.views.register import RegisterView
 from app.views.login import LoginView
 from app.views.logout import LogoutView
+from app.views.confirm import ConfirmView
 
 
 bp.add_url_rule("/register",
@@ -12,3 +13,6 @@ bp.add_url_rule("/log",
 
 bp.add_url_rule("/logout",
                 view_func=LogoutView.as_view("logout"))
+
+bp.add_url_rule("/confirm/<token>",
+                view_func=ConfirmView.as_view("confirm"))
