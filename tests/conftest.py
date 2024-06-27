848308e718b8548220f790a0e337fdade394bcd7
Iyed <iyedabdelli73@gmail.com>
2024-06-27 15:15:20 +0100

    Add tests for creating & updating spaceships, launch sites and launches

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from app import create_app, db
 from config import TestingConfig
+from app.models import User
 
 
 @pytest.fixture()
@@ -20,3 +21,24 @@ def app():
 @pytest.fixture()
 def client(app):
     return app.test_client()
+
+
+@pytest.fixture()
+def admin(app):
+    user = User(username="admin", email="admin@admin.com", role="admin")
+    user.set_password("password")
+    with app.app_context():
+        db.session.add(user)
+        db.session.commit()
+        db.session.refresh(user)
+    return user
+
+
+@pytest.fixture()
+def login_admin(client, admin):
+    with client:
+        client.post("/authentication/login", data={
+            "username": "admin",
+            "password": "password",
+        })
+        return client
