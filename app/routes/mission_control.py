49509f6c6b9103af1e637e040daa7ab95918c359
Iyed <iyedabdelli73@gmail.com>
2024-06-30 19:42:17 +0100

    Change mission control routes

--- app/routes/mission_control.py
+++ app/routes/mission_control.py
@@ -1,45 +1,51 @@
 from app.blueprints import mission_control_bp as bp
-from app.views.mission_control.add_objects import AddSpaceshipView, AddLaunchSiteView, AddLaunchView
-from app.views.mission_control.list_objects import ListSpaceshipsView, ListLaunchSitesView, ListLaunchesView
-from app.views.mission_control.update_objects import UpdateSpaceshipView, UpdateLaunchSiteView, UpdateLaunchView
-from app.views.mission_control.delete_objects import DeleteSpaceshipView, DeleteLaunchSiteView, DeleteLaunchView
+from app.views.mission_control.create_spaceship import CreateSpaceshipView
+from app.views.mission_control.create_launchsite import CreateLaunchSiteView
+from app.views.mission_control.create_launch import CreateLaunchView
+from app.views.mission_control.update_launch import UpdateLaunchView
+from app.views.mission_control.update_launchsite import UpdateLaunchSiteView
+from app.views.mission_control.update_spaceship import UpdateSpaceshipView
+from app.views.mission_control.list_items import ListSpaceshipsView, ListLaunchSitesView, ListLaunchesView
+from app.views.mission_control.delete_spaceship import DeleteSpaceshipView
+from app.views.mission_control.delete_launch import DeleteLaunchView
+from app.views.mission_control.delete_launchsite import DeleteLaunchSiteView
 from app.views.mission_control.panel import MissionControlView
 
 bp.add_url_rule("/panel",
-                view_func=MissionControlView.as_view("panel"))
+                   view_func=MissionControlView.as_view("panel"))
 
-bp.add_url_rule("/spaceship/add",
-                view_func=AddSpaceshipView.as_view("add_spaceship"))
+bp.add_url_rule("/spaceship/create",
+                   view_func=CreateSpaceshipView.as_view("create_spaceship"))
 
-bp.add_url_rule("/spaceship/list",
-                view_func=ListSpaceshipsView.as_view("list_spaceships"))
+bp.add_url_rule("/spaceship",
+                   view_func=ListSpaceshipsView.as_view("list_spaceships"))
 
-bp.add_url_rule("/spaceship/update/<int:id>",
-                view_func=UpdateSpaceshipView.as_view("update_spaceship"))
+bp.add_url_rule("/spaceship/<int:id>",
+                   view_func=UpdateSpaceshipView.as_view("update_spaceship"))
 
-bp.add_url_rule("/spaceship/delete/<int:id>",
-                view_func=DeleteSpaceshipView.as_view("delete_spaceship"))
+bp.add_url_rule("/spaceship/<int:id>/delete",
+                   view_func=DeleteSpaceshipView.as_view("delete_spaceship"))
 
-bp.add_url_rule("/launchsite/add",
-                view_func=AddLaunchSiteView.as_view("add_launchsite"))
+bp.add_url_rule("/launchsite/create",
+                   view_func=CreateLaunchSiteView.as_view("create_launchsite"))
 
-bp.add_url_rule("/launchsite/list",
-                view_func=ListLaunchSitesView.as_view("list_launchsites"))
+bp.add_url_rule("/launchsite",
+                   view_func=ListLaunchSitesView.as_view("list_launchsites"))
 
-bp.add_url_rule("/launchsite/update/<int:id>",
-                view_func=UpdateLaunchSiteView.as_view("update_launchsite"))
+bp.add_url_rule("/launchsite/<int:id>",
+                   view_func=UpdateLaunchSiteView.as_view("update_launchsite"))
 
-bp.add_url_rule("/launchsite/delete/<int:id>",
-                view_func=DeleteLaunchSiteView.as_view("delete_launchsite"))
+bp.add_url_rule("/launchsite/<int:id>/delete",
+                   view_func=DeleteLaunchSiteView.as_view("delete_launchsite"))
 
-bp.add_url_rule("/launch/add",
-                view_func=AddLaunchView.as_view("add_launch"))
+bp.add_url_rule("/launch/create",
+                   view_func=CreateLaunchView.as_view("create_launch"))
 
-bp.add_url_rule("/launch/list",
-                view_func=ListLaunchesView.as_view("list_launches"))
+bp.add_url_rule("/launch",
+                   view_func=ListLaunchesView.as_view("list_launches"))
 
-bp.add_url_rule("/launch/update/<uuid:id>",
-                view_func=UpdateLaunchView.as_view("update_launch"))
+bp.add_url_rule("/launch/<uuid:id>",
+                   view_func=UpdateLaunchView.as_view("update_launch"))
 
-bp.add_url_rule("/launch/delete/<uuid:id>",
-                view_func=DeleteLaunchView.as_view("delete_launch"))
+bp.add_url_rule("/launch/<uuid:id>/delete",
+                   view_func=DeleteLaunchView.as_view("delete_launch"))
