bddc035504d68d4e281039f81a3b8c6122b759ba
Iyed <iyedabdelli73@gmail.com>
2024-07-03 16:34:05 +0100

    Change routes for upcoming & past launches

--- app/routes/launches.py
+++ app/routes/launches.py
@@ -3,8 +3,8 @@ from app.views.launches.upcoming_launches import UpcomingLaunchesView
 from app.views.launches.past_launches import PastLaunchesView
 
 
-bp.add_url_rule("/upcoming-launches",
+bp.add_url_rule("/launches",
                 view_func=UpcomingLaunchesView.as_view("upcoming_launches"))
 
-bp.add_url_rule("/past-launches",
+bp.add_url_rule("/launches/past",
                 view_func=PastLaunchesView.as_view("past_launches"))
