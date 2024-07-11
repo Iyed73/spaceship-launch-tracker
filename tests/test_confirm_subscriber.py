46ac8906f0d3d57765759f5e01e9c9ec54e8e2c7
Iyed <iyedabdelli73@gmail.com>
2024-07-11 18:22:11 +0100

    Add tests for subscribing

---
+++ tests/test_confirm_subscriber.py
@@ -0,0 +1,28 @@
+from itsdangerous import URLSafeTimedSerializer
+from app.models import Subscriber
+from app import db
+from sqlalchemy import select
+
+
+def test_confirm_email(client, app):
+    serializer = URLSafeTimedSerializer(app.config["SECRET_KEY"])
+    token = serializer.dumps("test@test.com", salt=app.config["SECURITY_PASSWORD_SALT"])
+
+    subscriber = Subscriber(email="test@test.com")
+    with app.app_context():
+        db.session.add(subscriber)
+        db.session.commit()
+
+    response = client.get(f"/subscription/confirm/{token}", follow_redirects=True)
+
+    assert response.status_code == 200
+    assert b"You have confirmed your account. Thanks!" in response.data
+
+    with app.app_context():
+        subscriber = db.session.scalar(select(Subscriber).where(Subscriber.email == "test@test.com"))
+        assert subscriber.is_confirmed
+
+
+def test_confirmation_invalid(client):
+    response = client.get("/subscription/confirm/invalid_token", follow_redirects=True)
+    assert b"The confirmation link is invalid or expired" in response.data
---
+++ tests/test_subscribe.py
@@ -0,0 +1,35 @@
+import pytest
+from app.models import Subscriber
+from app import db
+
+
+def test_subscribe_success(client, app):
+    response = client.post("/subscription/subscribe",
+                           data={"email": "test@test.com"})
+    assert response.status_code == 302
+    with app.app_context():
+        assert Subscriber.query.count() == 1
+        assert Subscriber.query.first().email == "test@test.com"
+
+
+def test_subscribe_fails_invalid_form(client, app):
+    response = client.post("/subscription/subscribe",
+                           data={"email": "tes.com"})
+    assert b'Invalid email address.' in response.data
+
+
+def test_subscribe_fails_email_used(client, app, mocker):
+    subscriber = Subscriber(email="test@test.com")
+    with app.app_context():
+        db.session.add(subscriber)
+        db.session.commit()
+
+    mocker.patch("app.views.authentication.register.db.session.add")
+    mocker.patch("app.views.authentication.register.db.session.commit")
+
+    response = client.post("/authentication/register",
+                           data={"email": "test@test.com"})
+
+    assert response.status_code == 200
+    assert not db.session.add.called
+    assert not db.session.commit.called
