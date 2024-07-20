6a20ccce1d451d0fbf4d169d1494d2c9c81b6023
Iyed <iyedabdelli73@gmail.com>
2024-07-20 16:12:07 +0100

    Update create, delete and update launch views

--- app/views/mission_control/create_launch.py
+++ app/views/mission_control/create_launch.py
@@ -1,10 +1,13 @@
 from flask.views import MethodView
 from flask import render_template, flash, url_for, redirect
 from flask_login import current_user
-from app.models import Launch, Spaceship, LaunchSite
+from app.models import Launch, Spaceship, LaunchSite, LaunchEvent
 from app.forms import LaunchForm
 from app import db
 from app.decorators import admin_required
+from flask import current_app
+from app.tasks.process_event import process_subscribers_event
+from app.tasks.event_categories import EventCategory
 
 
 class CreateLaunchView(MethodView):
@@ -15,6 +18,13 @@ class CreateLaunchView(MethodView):
         self.form.spaceship_id.choices = [(spaceship.id, spaceship.name) for spaceship in Spaceship.query.all()]
         self.form.launch_site_id.choices = [(site.id, site.name) for site in LaunchSite.query.all()]
 
+    @staticmethod
+    def create_event(launch):
+        job = current_app.task_queue.enqueue(process_subscribers_event, EventCategory.NEW_LAUNCH, launch=launch)
+        event = LaunchEvent(id=job.get_id(), name=f"new launch: {launch.mission}", launch=launch, category=EventCategory.NEW_LAUNCH)
+        db.session.add(event)
+        db.session.commit()
+
     def get(self):
         return render_template("mission_control/create_object.html",
                                title="Create Launch",
@@ -24,11 +34,12 @@ class CreateLaunchView(MethodView):
     def post(self):
         form = self.form
         if form.validate_on_submit() and current_user.is_authenticated:
-            obj = Launch()
-            form.populate_obj(obj)
-            obj.creator_id = current_user.id
-            db.session.add(obj)
+            launch = Launch()
+            form.populate_obj(launch)
+            launch.creator_id = current_user.id
+            db.session.add(launch)
             db.session.commit()
+            self.create_event(launch)
             flash("Launch Created successfully!", "success")
             return redirect(url_for("mission_control.list_launches"))
         return render_template("mission_control/create_object.html",
--- app/views/mission_control/delete_launch.py
+++ app/views/mission_control/delete_launch.py
@@ -1,16 +1,27 @@
 from flask import redirect, url_for, flash
 from flask.views import MethodView
-from app.models import Launch
+from app.models import Launch, LaunchEvent
 from app import db
+from flask import current_app
 from app.decorators import admin_required
+from app.tasks.process_event import process_subscribers_event
+from app.tasks.event_categories import EventCategory
 
 
 class DeleteLaunchView(MethodView):
     decorators = [admin_required]
 
-    def post(self, id):
-        obj = Launch.query.get_or_404(id)
-        db.session.delete(obj)
+    @staticmethod
+    def create_event(launch):
+        job = current_app.task_queue.enqueue(process_subscribers_event, EventCategory.LAUNCH_DELETE, launch=launch)
+        event = LaunchEvent(id=job.get_id(), name=f"delete launch: {launch.mission}", launch=launch, category=EventCategory.LAUNCH_DELETE)
+        db.session.add(event)
         db.session.commit()
+
+    def post(self, id):
+        launch = Launch.query.get_or_404(id)
+        db.session.delete(launch)
+        db.session.commit()
+        self.create_event(launch)
         flash("Launch deleted successfully!", "success")
         return redirect(url_for("mission_control.list_launches"))
